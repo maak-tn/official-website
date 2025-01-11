000790711903d876d4b566695dfe67ae07ef8b41
aymkh <aymen4real@gmail.com>
2025-01-11 22:14:48 +0100

    chore: placeholder for work page

---
+++ src/app/work/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div className='flex items-center justify-center capitalize'>
+            our work here
+        </div>
+    )
+}
+
+export default page
