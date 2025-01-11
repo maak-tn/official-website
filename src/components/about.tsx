355fe9d988a73481888765568028d2387ac340ac
aymkh <aymen4real@gmail.com>
2025-01-11 22:57:39 +0100

    fix: about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,11 +19,13 @@ function About() {
     return (
         <section className='px-4 lg:py-0 py-12 w-full flex lg:flex-row flex-col lg:gap-0 gap-8 justify-between lg:items-center h-sahredSection'>
             <SectionTitle title='what i do ?' />
-            <div className='w-full h-full flex flex-col gap-24'>
-                <p className='lg:text-2xl lg:leading-loose lg:text-left text-justify lg:tracking-wide'>
-                    For over 6 years, I've helped small businesses and startups save up to 70% on web development costs through strategic use of <strong>no-code tools and custom development</strong>. From e-commerce websites to business automation, my proven hybrid approach delivers <strong>enterprise-grade solutions at a fraction of traditional development costs</strong>. Perfect for businesses looking for quick deployment and budget-friendly digital transformation with trusted expertise.
-                </p>
-
+            <div className='w-full h-full flex flex-col gap-24 lg:justify-center'>
+                <div className='lg:text-2xl lg:leading-loose lg:text-left text-justify lg:tracking-wide'>
+                    <p>
+                        For over 6 years, I've helped small businesses and startups save up to 70% on web development costs through strategic use of <strong>no-code tools and custom development</strong>. From e-commerce websites to business automation, my proven hybrid approach delivers <strong>enterprise-grade solutions at a fraction of traditional development costs</strong>.
+                    </p>
+                    <p>Perfect for businesses looking for quick deployment and budget-friendly digital transformation with trusted expertise.</p>
+                </div>
                 <div className='flex lg:flex-row flex-col lg:gap-24 gap-8 items-center'>
                     {
                         stats.map((item: TStatsCard, index: number) => (
