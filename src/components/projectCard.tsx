4b8f8a9ace7bb6c42785936d245a9ba0f39e9eec
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-03-26 21:03:48 +0100

    fix: added missing tags to project cards

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -12,6 +12,22 @@ export default function ProjectCard({ project, isReverse }: { project: TProject,
                 <h1 className='lg:text-5xl text-2xl font-bold capitalize'>{project.title}</h1>
                 {project.client && <h3 className='font-semibold text-xl'>For {project.client}</h3>}
                 <p className='text-base font-light'>{project.description}</p>
+                <div className='flex flex-col gap-1'>
+                    <div className='flex gap-1 flex-wrap'>
+                        {
+                            project.tags.map((tag: string, index: number) => (
+                                <span key={index} className='bg-black text-white opacity-70 px-2 py-1 rounded-lg'>#{tag}</span>
+                            ))
+                        }
+                    </div>
+                    <div className='flex gap-1 flex-wrap'>
+                        {
+                            project.techStack.map((tech: string, index: number) => (
+                                <span key={index} className='bg-black text-white opacity-70 px-2 py-1 rounded-lg'>#{tech}</span>
+                            ))
+                        }
+                    </div>
+                </div>
                 {
                     project.link &&
                     <Link href={'work/' + project.id} className='bg-white text-black px-4 py-2 rounded-lg lg:w-fit w-full text-center hover:bg-stone-200'>Read more</Link>
