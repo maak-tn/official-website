ccbd608923bb8ade290c005c914e91fd0bbc79f3
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-03-26 21:13:12 +0100

    chore: nav restyled

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -17,16 +17,16 @@ function Nav() {
             name: 'Work',
             link: '/work'
         },
-        {
-            name: 'Services',
-            link: '/services'
-        }
+        // {
+        //     name: 'Services',
+        //     link: '/services'
+        // }
     ];
 
     return (
         <nav className='sticky top-0 flex flex-col items-center justify-between w-full gap-4 p-4 lg:gap-0 lg:flex-row bg-background min-h-[var(--navbar-height)]'>
             <h1 className='text-5xl font-bold lg:text-xl'>MAAK CORP</h1>
-            <div className='flex gap-8'>
+            <div className='flex gap-4'>
                 {routes.map((route, index) => (
                     <Link href={route.link} key={index} className='font-light text-white uppercase'>{route.name}</Link>
                 ))}
